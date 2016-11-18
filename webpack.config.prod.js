e796b7516d92fd3bda10c77744d6c7e72217561c
David Ra Youssef <davidradeveloper@gmail.com>
2016-11-18 13:14:31 -0500

    Configure html-webpack-plugin to handle index.html and minify on production build

--- webpack.config.prod.js
+++ webpack.config.prod.js
@@ -1,4 +1,5 @@
 var webpack = require('webpack');
+var HtmlWebpackPlugin = require('html-webpack-plugin');
 
 module.exports = {
 	entry: './src/main.js',
@@ -12,11 +13,45 @@ module.exports = {
 				test: /\.js$/,
 				exclude: /node_modules/,
 				loader: 'babel'
-			}
+			},
+      {
+        test: /\.css$/,
+        loader: 'style-loader!css-loader'
+      }
 		]
 	},
 	plugins: [
+		new HtmlWebpackPlugin({
+			inject: true,
+			template: './src/index.html',
+			minify: {
+				removeComments: true,
+				collapseWhitespace: true,
+				removeRedundantAttributes: true,
+				useShortDoctype: true,
+				removeEmptyAttributes: true,
+				removeStyleLinkTypeAttributes: true,
+				keepClosingSlash: true,
+				minifyJS: true,
+				minifyCSS: true,
+				minifyURLs: true
+			}
+		}),
+		new webpack.DefinePlugin({ 'process.env.NODE_ENV': '"production"' }),
 		new webpack.optimize.OccurenceOrderPlugin(),
-		new webpack.optimize.DedupePlugin()
+		new webpack.optimize.DedupePlugin(),
+		new webpack.optimize.UglifyJsPlugin({
+			compress: {
+				screw_ie8: true,
+				warnings: false
+			},
+			mangle: {
+				screw_ie8: true
+			},
+			output: {
+				comments: false,
+				screw_ie8: true
+			}
+		})
 	]
 };
