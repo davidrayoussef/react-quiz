00d993a38cf1caf7d2e1a85fd3e96b63c3d32d59
David Ra Youssef <davidradeveloper@gmail.com>
2017-10-12 12:22:02 -0400

    Refactor shuffle

--- src/helpers/shuffleQuestions.js
+++ src/helpers/shuffleQuestions.js
@@ -1,11 +1,6 @@
-const shuffleQuestions = array => {
-  return array
-    .map((item, index, array) => {
-      return array[Math.floor(Math.random() * array.length)];
-    })
-    .filter((item, index, array) => {
-      return index === array.indexOf(item);
-    })
+const shuffleQuestions = (arr) => {
+  return arr
+    .sort((a,b) => Math.random() < .5 ? 1 : -1)
     .slice(0,10);
 };
 
