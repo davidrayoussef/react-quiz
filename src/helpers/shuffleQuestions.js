be90abb1e05792967586d98f38b5a94207f2d283
Galeel Bhasha Satthar <galeels-ttltw@thetrainline.com>
2018-01-18 21:40:33 +0530

    number of questions to be displayed will be received as props and defaulted to qauestions array length

--- src/helpers/shuffleQuestions.js
+++ src/helpers/shuffleQuestions.js
@@ -1,7 +1,7 @@
-const shuffleQuestions = (arr) => {
+const shuffleQuestions = (arr, limit=arr.length) => {
   return arr
     .sort((a,b) => Math.random() < .5 ? 1 : -1)
-    .slice(0,10);
+    .slice(0, limit);
 };
 
 export default shuffleQuestions;
